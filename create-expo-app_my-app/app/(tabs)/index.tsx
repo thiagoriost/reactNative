ab015d5a4ac107d888f200241147e4b5c9999987
thiagoriost <thiagoriostapia@gmail.com>
2026-04-03 19:33:29 -0500

    implement fonts 2

--- create-expo-app_my-app/app/(tabs)/index.tsx
+++ create-expo-app_my-app/app/(tabs)/index.tsx
@@ -8,13 +8,11 @@ const SafeAreaView = styled(RNSafeAreaView)
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind RIGO....!
-      </Text>
-      <Link href="/onboarding" className="mt-4 bg-primary text-white p-4 rounded-2xl">Go to Onboarding</Link>
-      <Link href="/otherPAgeToTest" className="mt-4 bg-primary text-white p-4 rounded-2xl">Go to Other page to test</Link>
-      <Link href="/(auth)/sign-in" className="mt-4 bg-primary text-white p-4 rounded-2xl">Go to Sign In</Link>
-      <Link href="/(auth)/sign-up" className="mt-4 bg-primary text-white p-4 rounded-2xl">Go to Sign Up</Link>
+      <Text className="text-5xl font-sans-bold"> RIGO....!</Text>
+      <Link href="/onboarding" className="mt-4 font-sans-bold bg-primary text-white p-4 rounded-2xl">Go to Onboarding</Link>
+      <Link href="/otherPAgeToTest" className="mt-4 font-sans-bold bg-primary text-white p-4 rounded-2xl">Go to Other page to test</Link>
+      <Link href="/(auth)/sign-in" className="mt-4 font-sans-bold bg-primary text-white p-4 rounded-2xl">Go to Sign In</Link>
+      <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold bg-primary text-white p-4 rounded-2xl">Go to Sign Up</Link>
 
       <Link href="/subscriptions/spotify" className="mt-4 bg-primary text-white p-4 rounded-2xl">Go to Subscriptions spotify</Link>
       <Link href={{
